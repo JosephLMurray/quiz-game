cc75f5d20c333ae3fb802273f1700379639f150c
Joseph Murray <josephl.murrayjr@gmail.com>
2022-04-03 15:45:57 -0400

    added timer

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,14 +1,14 @@
 const questions = [
     {question: 'What does JSON stand?',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
-    {question: 'What does JSON stand for 2',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
-    {question: 'What does JSON stand for 3',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
-    {question: 'What does JSON stand for 4',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
-    {question: 'What does JSON stand for 5',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
-    {question: 'What does JSON stand for 6',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
-    {question: 'What does JSON stand for 7',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
-    {question: 'What does JSON stand for 8',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
-    {question: 'What does JSON stand for 9',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
-    {question: 'What does JSON stand for 10',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
+    {question: 'What does JSON stand for 2',  possibles: ['JavaScript Object Notation', 'whatever', 'yep', 'you know it'], answer: 'JavaScript Object Notation'},
+    {question: 'What does JSON stand for 3',  possibles: ['JavaScript Object Notation', 'whatever', 'yep', 'you know it'], answer: 'JavaScript Object Notation'},
+    {question: 'What does JSON stand for 4',  possibles: ['JavaScript Object Notation', 'whatever', 'yep', 'you know it'], answer: 'JavaScript Object Notation'},
+    {question: 'What does JSON stand for 5',  possibles: ['JavaScript Object Notation', 'whatever', 'yep', 'you know it'], answer: 'JavaScript Object Notation'},
+    {question: 'What does JSON stand for 6',  possibles: ['JavaScript Object Notation', 'whatever', 'yep', 'you know it'], answer: 'JavaScript Object Notation'},
+    {question: 'What does JSON stand for 7',  possibles: ['JavaScript Object Notation', 'whatever', 'yep', 'you know it'], answer: 'JavaScript Object Notation'},
+    {question: 'What does JSON stand for 8',  possibles: ['JavaScript Object Notation', 'whatever', 'yep', 'you know it'], answer: 'JavaScript Object Notation'},
+    {question: 'What does JSON stand for 9',  possibles: ['JavaScript Object Notation', 'whatever', 'yep', 'you know it'], answer: 'JavaScript Object Notation'},
+    {question: 'What does JSON stand for 10',  possibles: ['JavaScript Object Notation', 'whatever', 'yep', 'you know it'], answer: 'JavaScript Object Notation'},
 ]
 
 var question = document.getElementById("questionField")
@@ -22,6 +22,7 @@ var scoresButton = document.getElementById("scores");
 var returnButton = document.getElementById("return-button");
 var playerName = document.getElementById("playerName");
 var playerScore = document.getElementById("playerScore");
+var timer = document.getElementById("timer-count");
 
 
     let questionObj = questions.splice(Math.floor(Math.random()*questions.length), 1).pop();
@@ -34,10 +35,24 @@ var playerScore = document.getElementById("playerScore");
 // console.log (pop)
 // a possibles[x]
 // b 
-scoresButton.addEventListener("click", function() {
-    location.href= "./Assets/index.html";
+startButton.addEventListener("click", function(event){
+    event.preventDefault();
+    countdown();
+    
 });
 
-returnButton.addEventListener("click", function() {
-    location.href= "../index.html";
-});
+function countdown() {
+
+    var timeLeft = 60;
+      
+    var timeInterval = setInterval(function () {
+        timeLeft--;
+        console.log(timer.textContent = timeLeft);
+        timer.textContent = timeLeft;
+  
+      if(timeLeft === 0) {
+        clearInterval(timeInterval);
+      }
+      //
+    }, 1000);
+  }
