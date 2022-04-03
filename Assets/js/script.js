a22f59686ed23e8caa5264c3713864da9c7ecf83
Joseph Murray <josephl.murrayjr@gmail.com>
2022-04-03 14:19:17 -0400

    more page styling

---
+++ Assets/js/script.js
@@ -0,0 +1,23 @@
+const questions = [
+    {question: 'What does JSON stand?',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
+    {question: 'What does JSON stand for 2',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
+    {question: 'What does JSON stand for 3',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
+    {question: 'What does JSON stand for 4',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
+    {question: 'What does JSON stand for 5',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
+    {question: 'What does JSON stand for 6',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
+    {question: 'What does JSON stand for 7',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
+    {question: 'What does JSON stand for 8',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
+    {question: 'What does JSON stand for 9',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
+    {question: 'What does JSON stand for 10',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
+]
+
+    let questionObj = questions.splice(Math.floor(Math.random()*questions.length), 1).pop();
+
+console.log(questionObj.answer)
+
+if (GamepadButton.value === questionObj.answer)
+
+populate buttons = for (questionObj.possibles.splice(Math.floor(Math.random()*questionObj.possibles.length), 1);)
+console.log (pop)
+a possibles[x]
+b 
