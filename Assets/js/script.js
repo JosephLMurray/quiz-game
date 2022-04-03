b0cc56afbef38a7ec3c4539c3460e6d9bf22565a
Joseph Murray <josephl.murrayjr@gmail.com>
2022-04-03 15:00:49 -0400

    adding js variables

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -11,13 +11,33 @@ const questions = [
     {question: 'What does JSON stand for 10',  possibles: ['JavaScript Object Notation', 'whatever'], answer: 'JavaScript Object Notation'},
 ]
 
+var question = document.getElementById("questionField")
+var poss1 = document.getElementById("poss1");
+var poss2 = document.getElementById("poss2");
+var poss3 = document.getElementById("poss3");
+var poss4 = document.getElementById("poss4");
+var timerText = document.getElementsByClassName("timer-text");
+var startButton = document.getElementById("start-button");
+var scoresButton = document.getElementById("scores");
+var returnButton = document.getElementById("return-button");
+var playerName = document.getElementById("playerName");
+var playerScore = document.getElementById("playerScore");
+
+
     let questionObj = questions.splice(Math.floor(Math.random()*questions.length), 1).pop();
 
-console.log(questionObj.answer)
+// console.log(questionObj.answer)
 
-if (GamepadButton.value === questionObj.answer)
+// if (GamepadButton.value === questionObj.answer)
 
-populate buttons = for (questionObj.possibles.splice(Math.floor(Math.random()*questionObj.possibles.length), 1);)
-console.log (pop)
-a possibles[x]
-b 
+// populate buttons = for (questionObj.possibles.splice(Math.floor(Math.random()*questionObj.possibles.length), 1);)
+// console.log (pop)
+// a possibles[x]
+// b 
+scoresButton.addEventListener("click", function() {
+    location.href= "./Assets/index.html";
+});
+
+returnButton.addEventListener("click", function() {
+    location.href= "../index.html";
+});
---
+++ Assets/js/script2.js
@@ -0,0 +1,7 @@
+var returnButton = document.getElementById("return-button");
+var playerName = document.getElementById("playerName");
+var playerScore = document.getElementById("playerScore");
+
+returnButton.addEventListener("click", function() {
+    location.href= "../index.html";
+});
