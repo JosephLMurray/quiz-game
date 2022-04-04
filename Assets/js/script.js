a5e82dac295d9bab644190dc53ef782959e77b2b
Joseph Murray <josephl.murrayjr@gmail.com>
2022-04-04 08:54:57 -0400

    structuring clicks and figuring out program order

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -12,7 +12,7 @@ const questions = [
 ]
 
 var question = document.getElementById("questionField");
-var answerfield = document.getElementsByClassName("answerFields");
+var answerFields = Array.from(document.getElementsByClassName("answerField"));
 var poss1 = document.getElementById("poss1");
 var poss2 = document.getElementById("poss2");
 var poss3 = document.getElementById("poss3");
@@ -24,74 +24,98 @@ var returnButton = document.getElementById("return-button");
 var playerName = document.getElementById("playerName");
 var playerScore = document.getElementById("playerScore");
 var timer = document.getElementById("timer-count");
+let currentQuestion;
+let timeInterval;
+let timeLeft = 60;
 
-
-// console.log(questionObj.answer)
-
-// if (GamepadButton.value === questionObj.answer)
-
-// populate buttons = for (questionObj.possibles.splice(Math.floor(Math.random()*questionObj.possibles.length), 1);)
-// console.log (pop)
-// a possibles[x]
-// b 
+//starts game
 startButton.addEventListener("click", function(event){
+    //prevents default navigation
     event.preventDefault();
+    //start timer
     countdown();
-    gameState();    
-});
+    //creates game question and options
+    gameUpdate();
+ });
 
-const gameState = () => {
 
+const gameUpdate = () => {
+    checkGameState();  
+    selectQuestion();
+    populateQuestion();
+    populatePossibles();    
 };
+
+const checkAnswer = (element) =>{
+    if (element.innerHTML !== currentQuestion.answer) {
+     timeLeft -= 2;
+    }
+    gameUpdate()
+ };
+
 const countdown = () => {
-  
-    var timeLeft = 60;
-      
-    var timeInterval = setInterval(function () {
+    timeInterval = setInterval(() => {
         timeLeft--;
-        var questionObj = selectQuestion();
-        console.log(questionObj);
-        populateFields();
         timer.textContent = timeLeft;
   
-      if(timeLeft === 0) {
-        clearInterval(timeInterval);
-      }
-      //
+        if(timeLeft === 0) {
+            clearInterval(timeInterval);
+            location.reload();
+        }
     }, 1000);
-  }
+}
 
 const selectQuestion = () => {
-    return questions.splice(Math.floor(Math.random()*questions.length), 1).pop();
+    let question = questions.splice(Math.floor(Math.random()*questions.length), 1).pop();
+    currentQuestion = question;
 };
 
 const populateQuestion = () => {
-    questionField.textContent = questionObj.question;
+    questionField.textContent = currentQuestion.question;
 };
 
 const populatePossibles = () => {
-    answerField.forEach(field => {
-        field.textContent= questionObj.possibles.splice(Math.floor(Math.random()*questionObj.possibles.length), 1);
+    answerFields.forEach( field => {
+        field.textContent = currentQuestion.possibles.splice(Math.floor(Math.random()*currentQuestion.possibles.length), 1);
     });
 };
 
-//check game state
-if (questions.length === 0) {
+const checkGameState = () => {
+    if (questions.length === 0) {
+        logHighScore();
+        location.reload;
+    }
+}
+
+const logHighScore = () => {
+    highScore = [];
+    console.log(highScore);
+    //grab highscores from local storage
+    highScore = localStorage.getItem("highScore", JSON.parse(highScore));
+    console.log(highScore);
+    //grab time for score
     playerScore = timer.textContent;
+    console.log(playerScore);
+    console.log(highScore);
+    //prompt for player name
     playerName = prompt("Congratulations! Enter your name:");
-
-//TODO: Create highScore array of Objects. sort by score. trim to 10 places. save to local storage.
-    highScore = [{pName:"", score:""}];
-
-    unshift.highScore({playerName, playerScore});
-
+    console.log(playerName);
+    console.log(highScore);
+    //trim playerName
+    playerName = playerName.trim();
+    console.log(playerName);
+    //check if highScore is empty
+    if (highScore === null) {
+        highScore = [{pName:"", score:""}];
+    }
+    //insert into High Scores array of objects
+    unshift.highScore({pName: playerName, score: playerScore});
+    //sort from high to low
     highScore.sort((a, b) => {
         return a.score - b.score;
     });
-
+    //trim to top 10 scores
     highScore = highScore.slice(0, 9);
-
+    //log highScore to local storage
     localStorage.setItem("highScore", JSON.stringify(highScore));
-
-
 }
